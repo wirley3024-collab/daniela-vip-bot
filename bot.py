868f78a81c7523715596b5af0529ac259d594407
wirley3024-collab <wirley.3024@gmail.com>
2025-08-14 00:04:07 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -203,7 +203,7 @@ def cb_ver_muestras(call):
     bot.send_message(chat_id, MUESTRAS_HEADER)
     for fid in PHOTOS:
         bot.send_photo(chat_id, fid)
-   bot.send_message(chat_id, MUESTRAS_FOOTER, reply_markup=kb_inicio(chat_id))
+bot.send_message(chat_id, MUESTRAS_FOOTER, reply_markup=kb_inicio(chat_id))
 
 
 def kb_inicio(chat_id):
@@ -398,6 +398,7 @@ if __name__ == "__main__":
 
     threading.Thread(target=daily_pruner, daemon=True).start()
     run_flask()
+
 
 
 
